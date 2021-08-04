b43e3fcf28f45f4d021304525a75074aca2677ba
Altug Bayram <altug@Altugs-MacBook-Air.local>
2021-08-04 22:15:13 +0300

    dollar is immutable

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,7 +9,9 @@ public class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar dollar = new Dollar(5);
-        dollar.times(2);
-        assertEquals(10, dollar.amount);
+        Dollar product = dollar.times(2);
+        assertEquals(10, product.amount);
+        product = dollar.times(3);
+        assertEquals(15, product.amount);
     }
 }
