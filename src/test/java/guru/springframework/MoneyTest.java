f1573da3a5a37f1a39d406901752f0e3523c833b
Altug Bayram <altug@Altugs-MacBook-Air.local>
2021-08-04 23:10:19 +0300

    added factory methods to money

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,32 +9,28 @@ public class MoneyTest {
 
     @Test
     void testMultiplicationDollar() {
-        Dollar dollar = new Dollar(5);
-        Dollar product = dollar.times(2);
-        assertEquals(new Dollar(10), product);
-        product = dollar.times(3);
-        assertEquals(new Dollar(15), product);
+        Money dollar = Money.dollar(5);
+        assertEquals(Money.dollar(10), dollar.times(2));
+        assertEquals(Money.dollar(15), dollar.times(3));
     }
 
     @Test
     void testEqualityDollar() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(8));
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
     void testMultiplicationFranc() {
-        Franc franc = new Franc(5);
-        Franc product = franc.times(2);
-        assertEquals(new Franc(10), product);
-        product = franc.times(3);
-        assertEquals(new Franc(15), product);
+        Money franc = Money.franc(5);
+        assertEquals(Money.franc(10), franc.times(2));
+        assertEquals(Money.franc(15), franc.times(3));
     }
 
     @Test
     void testEqualityFranc() {
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(5), new Franc(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(8));
     }
 }
